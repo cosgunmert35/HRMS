3c35aabe4a0ee5fc3d66d18823e6aaff08c1f4ab
Mert <cosgunmert35@gmail.com>
2021-05-29 22:39:53 +0300

    Employers adding.

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controller/EmployersController.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.EmployerService;
+import kodlamaio.hrms.entities.concretes.Employer;
+
+@RestController
+@RequestMapping("/api/employers")
+public class EmployersController {
+	
+	private EmployerService service;
+
+	public EmployersController(EmployerService service) {
+		super();
+		this.service = service;
+	}
+	
+	@RequestMapping("/getAll")
+	public List<Employer> getAll() {
+		
+		return this.service.getAll();
+	}
+	
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Employer.java
@@ -0,0 +1,29 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "employers")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employer {
+	
+	@Id
+	@Column(name = "user_id")
+	private int userId;
+	
+	@Column(name = "company_name")
+	private String companyName;
+	
+	@Column(name = "web_site")
+	private String webSite;
+
+}
