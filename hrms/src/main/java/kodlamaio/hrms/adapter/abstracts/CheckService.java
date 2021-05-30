dbea9a4ded8ed1061ec536f2b1fbd485775e204a
Mert <cosgunmert35@gmail.com>
2021-05-30 12:06:25 +0300

    Mernis service added.

---
+++ hrms/src/main/java/kodlamaio/hrms/adapter/abstracts/CheckService.java
@@ -0,0 +1,10 @@
+package kodlamaio.hrms.adapter.abstracts;
+
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+
+public interface CheckService {
+	
+	
+	public boolean checkIsPerson(JobSeeker jobSeeker);
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/adapter/concretes/MernisServiceAdapter.java
@@ -0,0 +1,34 @@
+package kodlamaio.hrms.adapter.concretes;
+
+import java.rmi.RemoteException;
+import java.time.LocalDate;
+import java.util.Locale;
+
+import kodlamaio.hrms.adapter.abstracts.CheckService;
+import kodlamaio.hrms.entities.concretes.JobSeeker;
+import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
+
+public class MernisServiceAdapter implements CheckService {
+
+	@Override
+	public boolean checkIsPerson(JobSeeker jobSeeker) {
+		KPSPublicSoapProxy client = new KPSPublicSoapProxy();
+		boolean result = true;
+		
+		LocalDate birthDate = LocalDate.parse((CharSequence) jobSeeker.getDateOfBirth());
+		 int year = birthDate.getYear();
+		
+		try {
+			result = client.TCKimlikNoDogrula(Long.parseLong(jobSeeker.getNationalityId()), jobSeeker.getFirstName().toUpperCase(new Locale("tr")),
+												jobSeeker.getLastName().toUpperCase(new Locale("tr")), year);
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return result;
+	}
+
+}
