a0fd5bbc273407c81056e27109ec35155516f3bc
Mert <cosgunmert35@gmail.com>
2021-05-31 17:35:25 +0300

    City added.

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controller/CitiesController.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.CityService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.City;
+
+@RestController
+@RequestMapping("api/cities")
+public class CitiesController {
+	
+	private CityService service;
+
+	public CitiesController(CityService service) {
+		super();
+		this.service = service;
+	}
+	
+	@GetMapping("/getAll")
+	public DataResult<List<City>> getAll() {
+		return this.service.getAll();
+	}
+	
+	
+}
