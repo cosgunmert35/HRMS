eddd6e5d6055787109eaf75b06e2ebf3e278affb
Mert <cosgunmert35@gmail.com>
2021-06-05 13:44:24 +0300

    School operations added.

---
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/SchoolManager.java
@@ -0,0 +1,36 @@
+package kodlamaio.hrms.business.concretes;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kodlamaio.hrms.business.abstracts.SchoolService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
+import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
+import kodlamaio.hrms.dataAccess.abstracts.SchoolDao;
+import kodlamaio.hrms.entities.concretes.School;
+
+@Service
+public class SchoolManager implements SchoolService {
+	
+	private SchoolDao schoolDao;
+	
+	public SchoolManager(SchoolDao schoolDao) {
+		super();
+		this.schoolDao = schoolDao;
+	}
+
+	@Override
+	public Result add(School school) {
+		this.schoolDao.save(school);
+		return new SuccessResult(school.getSchoolName() + " başarıyla eklendi.");
+	}
+
+	@Override
+	public DataResult<List<School>> getAll() {
+		return new SuccessDataResult<List<School>>(this.schoolDao.findAll(), "Okullar listesi getirildi.");
+	}
+
+}
---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/School.java
@@ -0,0 +1,46 @@
+package kodlamaio.hrms.entities.concretes;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "schools")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class School {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+	
+	@Column(name = "job_seeker_id")
+	private int jobSeekerId;
+	
+	@Column(name = "school_name")
+	private String schoolName;
+	
+	@Column(name = "department")
+	private String department;
+	
+	@Column(name = "start_date")
+	private LocalDate startDate;
+	
+	@Column(name = "date_of_graduation")
+	private LocalDate dateOfGraduation;
+	
+	@Column(name = "is_active")
+	private boolean isActive;
+
+}
