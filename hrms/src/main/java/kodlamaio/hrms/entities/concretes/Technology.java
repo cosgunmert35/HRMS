5bfc9a4b393d86d1ca56d4ea89f9178a03cdb77e
Mert <cosgunmert35@gmail.com>
2021-06-10 18:05:04 +0300

    Relationships added.

--- hrms/src/main/java/kodlamaio/hrms/entities/concretes/Technology.java
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/Technology.java
@@ -1,10 +1,13 @@
 package kodlamaio.hrms.entities.concretes;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -27,4 +30,6 @@ public class Technology {
 	@Column(name = "technology_name")
 	private String technologyName;
 
+	@OneToMany(mappedBy = "technology")
+	private  List<KnownTechnology> knownTechnologies;
 }
