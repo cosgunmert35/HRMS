763e6435f48193cc6543d393d98863db3896c69f
Mert <cosgunmert35@gmail.com>
2021-06-10 18:04:44 +0300

    Add operation added.

--- hrms/src/main/java/kodlamaio/hrms/api/controller/ResumesController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controller/ResumesController.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kodlamaio.hrms.business.abstracts.ResumeService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.entities.concretes.Resume;
 
 @RestController
@@ -23,6 +26,10 @@ public class ResumesController {
 		this.resumeService = resumeService;
 	}
 	
+	@PostMapping(value = "/add")
+	public Result add(@RequestBody Resume resume) {
+		return this.resumeService.add(resume);
+	}
 	
 	@GetMapping(value = "/getAll")
 	public DataResult<List<Resume>> getAll() {
--- hrms/src/main/java/kodlamaio/hrms/business/concretes/ResumeManager.java
+++ hrms/src/main/java/kodlamaio/hrms/business/concretes/ResumeManager.java
@@ -7,7 +7,9 @@ import org.springframework.stereotype.Service;
 
 import kodlamaio.hrms.business.abstracts.ResumeService;
 import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.core.utilities.results.Result;
 import kodlamaio.hrms.core.utilities.results.SuccessDataResult;
+import kodlamaio.hrms.core.utilities.results.SuccessResult;
 import kodlamaio.hrms.dataAccess.abstracts.ResumeDao;
 import kodlamaio.hrms.entities.concretes.Resume;
 
@@ -21,10 +23,20 @@ public class ResumeManager implements ResumeService {
 		super();
 		this.resumeDao = resumeDao;
 	}
-
+	
+	
+	@Override
+	public Result add(Resume resume) {
+		this.resumeDao.save(resume);
+		return new SuccessResult("Öz geçmiş eklendi.");
+	}
+	
+	
 	@Override
 	public DataResult<List<Resume>> getAll() {
 		return new SuccessDataResult<List<Resume>>(this.resumeDao.findAll());
 	}
+
+	
 
 }
