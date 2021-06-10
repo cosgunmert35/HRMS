55cb14639f36f30ec84704f32d5ce23a862a4558
Mert <cosgunmert35@gmail.com>
2021-06-10 15:29:08 +0300

    resume base operation added.

---
+++ hrms/src/main/java/kodlamaio/hrms/api/controller/ResumesController.java
@@ -0,0 +1,32 @@
+package kodlamaio.hrms.api.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kodlamaio.hrms.business.abstracts.ResumeService;
+import kodlamaio.hrms.core.utilities.results.DataResult;
+import kodlamaio.hrms.entities.concretes.Resume;
+
+@RestController
+@RequestMapping(value = "/api/resumes")
+public class ResumesController {
+	
+	private ResumeService resumeService;
+
+	@Autowired
+	public ResumesController(ResumeService resumeService) {
+		super();
+		this.resumeService = resumeService;
+	}
+	
+	
+	@GetMapping(value = "/getAll")
+	public DataResult<List<Resume>> getAll() {
+		return this.resumeService.getAll();
+	}
+
+}
