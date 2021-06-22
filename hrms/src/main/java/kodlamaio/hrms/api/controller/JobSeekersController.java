55480999098f275aa6e1097b8dc838e54cd0fe6e
Mert <cosgunmert35@gmail.com>
2021-06-22 09:32:33 +0300

    @CrossOrigin added.

--- hrms/src/main/java/kodlamaio/hrms/api/controller/JobSeekersController.java
+++ hrms/src/main/java/kodlamaio/hrms/api/controller/JobSeekersController.java
@@ -3,6 +3,7 @@ package kodlamaio.hrms.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,6 +18,7 @@ import kodlamaio.hrms.entities.concretes.JobSeeker;
 
 @RestController
 @RequestMapping(value = "/api/jobSeekers")
+@CrossOrigin
 public class JobSeekersController {
 
 	private JobSeekerService service;
